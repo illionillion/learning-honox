1296337c4dbb4b9dac0243424b1b7bef7d400700
yuto ichinose <yuto.0526.sano@gmail.com>
2024-03-17 10:22:34 +0900

    style: グローバルのスタイル設定

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,4 +1,4 @@
-import { Style } from 'hono/css'
+import { Style, css } from 'hono/css'
 import { jsxRenderer } from 'hono/jsx-renderer'
 import { Script } from 'honox/server'
 import { Layout } from '../../components/Layout'
@@ -11,7 +11,26 @@ export default jsxRenderer(({ children, title }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
         <Script src="/app/client.ts" async />
-        <Style />
+        <Style >
+          {css`
+            html {
+              font-size: 16px;
+              font-family: system-ui, sans-serif;
+            }
+            body {
+              min-height: 100vh;
+              color: #262626;
+              background-color: #f4f4f4;
+            }
+            *,
+            *::before,
+            *::after {
+              box-sizing: border-box;
+              margin: 0;
+              padding: 0;
+            }
+          }`}
+        </Style>
       </head>
       <body>
         <Layout>{children}</Layout>
