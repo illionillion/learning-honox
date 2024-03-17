c5641af34cfaf59fd2c3bf25eee83112cabb78e5
yuto ichinose <yuto.0526.sano@gmail.com>
2024-03-18 08:56:30 +0900

    feat: フォームのバリデーションチェック追加

--- app/routes/articles/create.tsx
+++ app/routes/articles/create.tsx
@@ -1,6 +1,8 @@
 import { css } from "hono/css";
 import { createRoute } from "honox/factory";
 import type { FC } from "hono/jsx";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 const titleClass = css`
   font-size: 1.5rem;
@@ -49,29 +51,88 @@ const buttonClass = css`
   }
 `;
 
-const Page: FC = () => {
-    return (
-        <div>
-            <h1 class={titleClass}>Create an article</h1>
-            <form class={formClass} method="post">
-                <label class={labelClass}>
-                    Title
-                    <input name="title" class={inputClass} type="text" />
-                </label>
-                <label class={labelClass}>
-                    Content
-                    <textarea name="content" class={textareaClass} />
-                </label>
-                <button class={buttonClass} type="submit">
-                    Create
-                </button>
-            </form>
-        </div>
-    );
+const errorClass = css`
+  color: red;
+  font-size: 0.75rem;
+`;
+
+type Data = {
+  title: {
+    value: string;
+    error: string[] | undefined;
+  };
+  content: {
+    value: string;
+    error: string[] | undefined;
+  };
+};
+
+type Props = {
+  data?: Data;
+};
+
+const Page: FC<Props> = ({ data }) => {
+  return (
+    <div>
+      <h1 class={titleClass}>Create an article</h1>
+      <form class={formClass} method="post">
+        <label class={labelClass}>
+          Title
+          <input name="title" class={inputClass} type="text"
+            value={data?.title.value}
+          />
+        </label>
+        {data?.title.error &&
+          data.title.error.map((error) => <p class={errorClass}>{error}</p>)}
+        <label class={labelClass}>
+          Content
+          <textarea name="content" class={textareaClass}
+            value={data?.content.value}
+          />
+        </label>
+        {data?.content.error &&
+          data.content.error.map((error) => <p class={errorClass}>{error}</p>)}
+        <button class={buttonClass} type="submit">
+          Create
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default createRoute((c) => {
-    return c.render(<Page />, {
-        title: "Create an article",
-    });
+  return c.render(<Page />, {
+    title: "Create an article",
+  });
 });
+
+const Article = z.object({
+  title: z.string().min(1).max(255),
+  content: z.string().min(1),
+});
+
+export const POST = createRoute(
+  zValidator("form", Article, (result, c) => {
+    if (result.success) {
+      // TODO DB に保存する
+      console.log(result.data);
+      return c.redirect("/articles");
+    }
+
+    const { title, content } = result.data;
+    const data: Data = {
+      title: {
+        value: title,
+        error: result.error.flatten().fieldErrors.title,
+      },
+      content: {
+        value: content,
+        error: result.error.flatten().fieldErrors.content,
+      },
+    };
+
+    return c.render(<Page {...{data}} />, {
+      title: "Create an article",
+    });
+  })
+);
